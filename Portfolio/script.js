6d3767349de0a45a2146ae3260f72320d75d3f4f
MouadNaji <44157184+MouadNaji@users.noreply.github.com>
2019-01-06 16:28:27 +0100

    Add files via upload

---
+++ Portfolio/script.js
@@ -0,0 +1,15 @@
+console.log("sker der dude");
+var menu = document.getElementById("Open-menu");
+var overlay = document.getElementById("overlay");
+let luk = document.getElementById("luk");
+
+menu.addEventListener("click", menuen);
+luk.addEventListener("click", lukDen)
+
+function lukDen() {
+    overlay.classList.remove("show")
+}
+
+function menuen() {
+    overlay.classList.add("show")
+}
