5d6dc607dd267ba7d4f44ee6e5c4150760b49d4a
Mouad Naji <mouadnaji@Mouads-MacBook-Pro.local>
2019-01-07 19:50:04 +0100

    l

--- script.js
+++ script.js
@@ -2,17 +2,17 @@ console.log("sker der");
 var menu = document.getElementById("Open-menu");
 var overlay = document.getElementById("overlay");
 var luk = document.getElementById("luk");
-var mouad = document.getElementById("mig");
+var mig = document.getElementById("mig");
 
 menu.addEventListener("click", menuen);
 luk.addEventListener("click", lukDen)
 
 function lukDen() {
     overlay.classList.remove("show")
-    mouad.classList.remove("hide")
+    mig.classList.remove("hide")
 }
 
 function menuen() {
     overlay.classList.add("show")
-    mouad.classList.add("hide")
+    mig.classList.add("hide")
 }
